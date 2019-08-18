ebe974ebd01cb70d536983c48181c930c659e730
yihuang <yi.codeplayer@gmail.com>
2019-08-18 23:27:43 +0800

    fix segfault

--- Crypto/Hash/Blake2b.hs
+++ Crypto/Hash/Blake2b.hs
@@ -101,7 +101,7 @@ instance HashAlgorithm Blake2b_512 where
 foreign import ccall unsafe "cryptonite_blake2b_init"
     c_blake2b_init :: Ptr (Context a) -> Word32 -> IO ()
 
-foreign import ccall "cryptonite_blake2b_update"
+foreign import ccall unsafe "cryptonite_blake2b_update"
     c_blake2b_update :: Ptr (Context a) -> Ptr Word8 -> Word32 -> IO ()
 
 foreign import ccall unsafe "cryptonite_blake2b_finalize"
